d4fcfd454976739fff6e5553e918081fd8392c82
Anton Ishkov <iskov.anton@yandex.com>
2014-12-12 15:16:05 +0100

    Add user on provision (MySQL 5.6)

--- scripts/mysql-5.6.py
+++ scripts/mysql-5.6.py
@@ -1,10 +1,13 @@
 from util import *
 
+root_password = '123456'
+
+user = 'root'
 password = '123456'
 
 # Configure mysql
-call('echo "mysql-server mysql-server/root_password password %s" | debconf-set-selections' % password)
-call('echo "mysql-server mysql-server/root_password_again password %s" | debconf-set-selections' % password)
+call('echo "mysql-server mysql-server/root_password password %s" | debconf-set-selections' % root_password)
+call('echo "mysql-server mysql-server/root_password_again password %s" | debconf-set-selections' % root_password)
 
 # Install packages from manager
 
@@ -16,4 +19,23 @@ echo('Post installation configuring MySQL...')
 replace('/etc/mysql/my.cnf', 'bind-address', '#ba:')
 chown('/etc/mysql/my.cnf', 'mysql', 'mysql')
 call('/etc/init.d/mysql restart')
+
+if user == 'oot':
+    # TODO: Add user
+    print 'User creation not implemented'
+
+queries = [
+    'create user %(user)s@10.0.2.2 identified by "%(password)s";',
+    'grant all privileges on *.* to %(user)s@10.0.2.2 with grant option;',
+    'flush privileges;',
+
+    'select user, host from mysql.user where user="%(user)s";'
+]
+
+for query in queries:
+    call("mysql -uroot -p%(password)s -e '%(query)s'" % {
+        "password": root_password,
+        "query": query % {"user": user, "password": password}
+    })
+
 echo('... Done')
