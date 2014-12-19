c9d6b8c429bb5592ca4a37fff427285e2691e24d
Anton Iškov <iskov.anton@yandex.com>
2014-12-19 09:53:13 +0100

    Update install.py

--- install.py
+++ install.py
@@ -1,9 +1,13 @@
 import urllib
 import json
 import os
+import os.path
 
 script_dir = 'scripts'
-os.mkdir('scripts', 0755)
+
+if not os.path.exists(script_dir):
+    os.mkdir(script_dir, 0755)
+
 base_url = 'https://raw.githubusercontent.com/aiskov/vm-config/develop'
 
 """
@@ -11,12 +15,13 @@ Downloading sample configuration file
 """
 urllib.urlretrieve("%s/config.json" % base_url, "config-sample.json")
 
-url = "https://api.github.com/repositories/aiskov/contents/scripts?ref=develop"
-
 """
 Downloading script files
 """
-for file in json.loads(urllib.urlopen(url).read()):
+url = "https://api.github.com/repositories/25528321/contents/scripts?ref=develop"
+files = json.loads(urllib.urlopen(url).read())
+
+for file in files:
     print 'Downloding - %s' % file['path']
     urllib.urlretrieve(file['download_url'], file['path'])
 
