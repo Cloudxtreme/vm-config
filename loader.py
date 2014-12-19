e4390d4a0dc3f4e72ea085a5c9c26137ad8b4c72
Anton Iškov <iskov.anton@yandex.com>
2014-12-19 10:43:41 +0100

    Update and rename install.py to loader.py

--- loader.py
+++ loader.py
@@ -10,12 +10,12 @@ if not os.path.exists(script_dir):
 
 base_url = 'https://raw.githubusercontent.com/aiskov/vm-config/develop'
 
-"""
+print """
 Downloading sample configuration file
 """
 urllib.urlretrieve("%s/config.json" % base_url, "config-sample.json")
 
-"""
+print """
 Downloading script files
 """
 url = "https://api.github.com/repositories/25528321/contents/scripts?ref=develop"
@@ -25,4 +25,5 @@ for file in files:
     print 'Downloding - %s' % file['path']
     urllib.urlretrieve(file['download_url'], file['path'])
 
+print '%s files downloaded' % len(files)
 print 'Preparation done'
