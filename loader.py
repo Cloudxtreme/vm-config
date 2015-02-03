e67d2142ce238aba4e05ba4e7c78e5eca7e9afc3
Anton Iškov <iskov.anton@yandex.com>
2015-02-03 21:59:07 +0100

    Update loader.py

--- loader.py
+++ loader.py
@@ -3,7 +3,7 @@ import json
 import os
 import os.path
 
-base_url = 'https://raw.githubusercontent.com/aiskov/vm-config/develop'
+base_url = 'https://raw.githubusercontent.com/aiskov/vm-config/master'
 
 print """
 Downloading sample configuration file
@@ -28,7 +28,7 @@ script_dir = 'scripts'
 if not os.path.exists(script_dir):
     os.mkdir(script_dir, 0755)
 
-url = "https://api.github.com/repositories/25528321/contents/scripts?ref=develop"
+url = "https://api.github.com/repositories/25528321/contents/scripts?ref=master"
 files = json.loads(urllib.urlopen(url).read())
 
 for prov_file in files:
