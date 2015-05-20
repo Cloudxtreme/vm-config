2ffefee2ca827f8f22aec831d1d889ac5d4808cc
Anton Iškov <iskov.anton@yandex.com>
2015-05-20 23:18:26 +0200

    Update mongodb.py

--- scripts/mongodb.py
+++ scripts/mongodb.py
@@ -7,7 +7,7 @@ call('apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10')
 call('echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list')
 call('apt-get update > /dev/null')
 
-if 'version' in load_config()['mongodb']):
+if 'version' in load_config()['mongodb']:
   call('apt-get install -y mongodb-org=%(v)s mongodb-org-server=%(v)s mongodb-org-shell=%(v)s mongodb-org-mongos=%(v)s mongodb-org-tools=%(v)s' % {'v': json_data['version']})
 else:
   call('apt-get install -y mongodb-org')
