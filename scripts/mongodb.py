29be9d7e06c170c2b5fc771324ab4e52cf97f191
Anton Iškov <iskov.anton@yandex.com>
2015-05-20 23:05:24 +0200

    Update mongodb.py

--- scripts/mongodb.py
+++ scripts/mongodb.py
@@ -3,7 +3,7 @@ from util import *
 json_data = load_config()['mongodb']
 
 call('apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10')
-call('echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | tee /etc/apt/sources.list.d/mongodb.list')
+call('echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list')
 call('apt-get update > /dev/null')
 install('mongodb-org=%(v)s mongodb-org-server=%(v)s mongodb-org-shell=%(v)s mongodb-org-mongos=%(v)s mongodb-org-tools=%(v)s' % {'v': json_data['version']})
 call('service mongod start')
