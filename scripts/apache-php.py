fdde3eda20a948fd74ff43b7b338e18d0f6965cd
Anton Iškov <iskov.anton@yandex.com>
2015-05-31 21:40:57 +0200

    Update apache-php.py

--- scripts/apache-php.py
+++ scripts/apache-php.py
@@ -7,12 +7,16 @@ install('apache2', 'php5', 'php-pear', 'php5-dev', 'php5-mysql', 'libapache2-mod
         'php5-mcrypt', 'php5-curl', 'php5-common', 'php5-cgi', 'php5-gd', 'php5-xdebug')
 
 # Post config php
-append(php_ini, 'extension=/usr/lib/php5/20121212/mcrypt.so\n')
-append(php_ini, 'zend_extension=/usr/lib/php5/20121212/xdebug.so\n')
+append(php_ini, 'extension=%s\n' % find_first('mcrypt.so', '/usr/lib/php5'))
+append(php_ini, 'zend_extension=%s\n' % find_first('xdebug.so', '/usr/lib/php5'))
+
 append(php_ini, 'xdebug.remote_enable=On\n')
+append(php_ini, 'xdebug.remote_connect_back=On\n')
+append(php_ini, 'xdebug.idekey="vagrant"\n')
 
 call('a2enmod rewrite')
 
-insert('/etc/apache2/apache2.conf', '\n        AllowOverride All', '<Directory /var/www/>')
+replace_after('/etc/apache2/apache2.conf', 'AllowOverride None', 'AllowOverride All', '<Directory /var/www/>')
 
-call('/etc/init.d/apache2 restart')
+call('service apache2 restart')
+call('service php5-fpm restart')
